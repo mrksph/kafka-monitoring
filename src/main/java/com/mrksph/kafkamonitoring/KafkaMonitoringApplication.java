c0206db1fd3f957adac0f33ebe59fb4931c42f7e
Marcos <markcosta15@hotmail.com>
2022-11-07 23:37:37 +0100

    root commit

---
+++ src/main/java/com/mrksph/kafkamonitoring/KafkaMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.mrksph.kafkamonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrksph/kafkamonitoring/KafkaMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrksph.kafkamonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
